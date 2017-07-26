477baa8adff43d1e37c22f19a3e964e8fbe51555
Kyle <kgerheiser@gmail.com>
2017-07-25 23:50:35 -0400

    Remove cell_list type because it is no longer needed as that functionality is handled by freecell in the tree itself like the original treecode does

--- src/cell_mod.f90
+++ src/cell_mod.f90
@@ -5,7 +5,7 @@ module cell_mod
   implicit none
 
   private
-  public :: cell, cell_ptr, cell_list
+  public :: cell, cell_ptr
 
   type, extends(node) :: cell
      real(prec) :: rcrit2, quad_moment(ndims,ndims)
@@ -17,31 +17,13 @@ module cell_mod
 
   type :: cell_ptr
      class(cell), pointer :: ptr => null()
-   contains
-     !procedure :: set_ptr
-     procedure :: get_desc_ptr
-     procedure :: set_desc_ptr
-     procedure :: sub_index
-     procedure :: has_child
   end type cell_ptr
 
   type :: cell_list_node
      type(cell_ptr) :: cellptr, next
   end type cell_list_node
 
-  type :: cell_list
-     type(cell_list_node) :: head 
-     type(cell_list_node) :: current
-     integer :: ncells
-   contains
-     procedure :: initialize
-     procedure :: reset
-     procedure :: get_free_cell
-  end type cell_list
-
 contains
-
-!  subroutine set_descendant_ptr(self, ptr, i)
 
   function get_desc_ptr(self, index) result(desc_ptr)
     class(cell_ptr), intent(in) :: self
@@ -75,12 +57,12 @@ contains
     class(cell_ptr), intent(in) :: self
     class(node), intent(in) :: b
 
-    integer :: i
+    integer :: k
     index = 0
     
-    do i = 1, ndims
-       if (self%ptr%pos(i) <= b%pos(i)) then
-          index = index + ishft(nsub, -i)
+    do k = 1, ndims
+       if (self%ptr%pos(k) <= b%pos(k)) then
+          index = index + ishft(nsub, -k)
        end if
     end do
     
@@ -110,32 +92,5 @@ contains
     class(cell_ptr), intent(in) :: cellptr
     is_null = .not. associated(cellptr%ptr)
   end function is_null
-
-  subroutine initialize(list)
-    class(cell_list), intent(inout) :: list
-    allocate(list%head%cellptr%ptr)
-    list%current%cellptr%ptr => list%head%cellptr%ptr
-    list%ncells = 0
-  end subroutine initialize
-
-  subroutine reset(list)
-    class(cell_list), intent(inout) :: list
-    list%current%cellptr%ptr => list%head%cellptr%ptr
-    list%ncells = 0
-  end subroutine reset
-  
-  function get_free_cell(list) result(freecell)
-    class(cell_list), intent(inout) :: list
-    type(cell), pointer :: freecell
- 
-    if (associated(list%current%cellptr%ptr)) then
-       freecell => list%current%cellptr%ptr
-    else
-       allocate(list%current%next%ptr)
-       list%current%next%ptr%type = cell_type
-    end if
-    
-    list%current%cellptr%ptr => list%current%next%ptr
-  end function get_free_cell
   
 end module cell_mod
