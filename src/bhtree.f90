0891939baa82b662d82528cbbc050cef22c07ffd
Kyle <kgerheiser@gmail.com>
2018-05-03 01:49:09 -0400

    New routine to read config directly or with a filename

--- src/bhtree.f90
+++ src/bhtree.f90
@@ -8,13 +8,14 @@ module bhtree_mod
   implicit none
 
   private
-  public :: bhtree, thread_tree, eval_quadrupole_moment, gravcalc, make_tree
+  public :: thread_tree, eval_quadrupole_moment, gravcalc, make_tree
 
   integer, parameter :: max_depth = 32
-
-  type :: bhtree
-     real(prec) :: theta, eps, eps2, rsize
-     logical :: quick_scan = .false., use_quad, bh86, sw94, first_call = .true., print_time = .false.
+  logical, parameter :: use_test = .false. 
+ 
+  type, public :: bhtree
+     real(prec) :: theta, eps2, rsize
+     logical :: quick_scan, use_quad, bh86, sw94, first_call = .true., print_time = .false.
      integer :: tdepth, ncells, nbbcalc, nbccalc, actmax, nbodies, actlen
      integer :: cell_hist(max_depth), subn_hist(max_depth)
      type(cell), pointer :: root => null()
@@ -25,29 +26,38 @@ module bhtree_mod
      procedure :: new_tree
      procedure :: load_body
      procedure :: eval_center_of_mass
-     procedure :: read_config
+     procedure :: read_config, read_config_file
      procedure :: make_tree
      procedure :: gravcalc
   end type bhtree
 
 contains
 
-  subroutine read_config(tree, file)
+  subroutine read_config_file(tree, file)
     class(bhtree), intent(inout) :: tree
     character(*), intent(in) :: file
 
     type(config) :: conf
-
+    
     call conf%read_file(file)
+    call tree%read_config(conf)
+    
+  end subroutine read_config_file
 
-    call conf%value_from_key("use_quad", tree%use_quad)
+  subroutine read_config(tree, conf)
+    class(bhtree), intent(inout) :: tree
+    type(config), intent(in) :: conf
+
     call conf%value_from_key("theta", tree%theta)
     call conf%value_from_key("eps", tree%eps2)
-    call conf%value_from_key("use_quad", tree%use_quad)
-    call conf%value_from_key("use_quad", tree%use_quad)
-    
+    tree%eps2 = tree%eps2**2
+    call conf%value_from_key("use_quad", tree%use_quad, default_value = .false.)
+    call conf%value_from_key("quick_scan", tree%quick_scan, default_value = .false.)
+    call conf%value_from_key("bh86", tree%bh86, default_value = .true.)
+    call conf%value_from_key("sw94", tree%sw94, default_value = .false.)
+
   end subroutine read_config
-  
+
   !*****************************************************************************
   ! Initialize tree for hierarchical force calculation
   !*****************************************************************************
@@ -60,7 +70,7 @@ contains
 
     call set_update(body_array)
     call tree%new_tree()
-    
+
     tree%root => tree%make_cell()
     tree%root%pos = 0.0_prec
     tree%root%center = 0.0_prec
@@ -88,7 +98,7 @@ contains
     if (tree%use_quad) then
        call eval_quadrupole_moment(tree%root)
     end if
-    
+
   end subroutine make_tree
 
   subroutine set_update(body_array)
@@ -100,9 +110,9 @@ contains
     do i = 1, n
        body_array(i)%ptr%update = .true.
     end do
-    
+
   end subroutine set_update
-  
+
   !*****************************************************************************
   ! Reclaim cells in tree, and prepare to build a new one
   !*****************************************************************************
@@ -129,10 +139,10 @@ contains
 
     tree%root => null()
     tree%ncells = 0
-    
+
   end subroutine new_tree
 
-  
+
   !*****************************************************************************
   ! Return a pointer to a free cell
   !*****************************************************************************
@@ -149,20 +159,20 @@ contains
        class is(cell)
           freecell => c
           tree%freecell => c%next
-       end select       
+       end select
     end if
-   
+
     freecell%update = .false.
-    
+
     do i = 1, nsub
        freecell%descendants(i)%ptr => null()
     end do
-    
+
     tree%ncells = tree%ncells + 1
-    
+
   end function make_cell
 
-  
+
   !*****************************************************************************
   ! Insert body into tree 
   !*****************************************************************************
@@ -179,13 +189,13 @@ contains
     qsize = tree%rsize
 
     do while(associated(q%descendants(q_index)%ptr))
-   
+
        select type(child => q%descendants(q_index)%ptr)   
        class is(body)
           dr = p%pos - child%pos
           dist2 = dot_product(dr, dr)
           if (dist2 == 0.0_prec) stop "two bodies have same position"
-          
+
           c => tree%make_cell()
           c%pos = q%pos + (merge(-qsize, qsize, p%pos < q%pos) / 4.0_prec)
           c%center = q%pos + (merge(-qsize, qsize, p%pos < q%pos) / 4.0_prec)
@@ -197,9 +207,9 @@ contains
 
        select type(child => q%descendants(q_index)%ptr)
        class is(cell)
-          q => child
+             q => child
        class default
-          stop "not a cell"
+             stop "not a cell"
        end select
 
        q_index = q%sub_index(p)
@@ -209,7 +219,7 @@ contains
     q%descendants(q_index)%ptr => p
 
   end subroutine load_body
-  
+
 
   !*****************************************************************************
   ! Recursive walk of tree installing next and more links
@@ -225,28 +235,28 @@ contains
 
     select type(p)
     type is(cell)
-       
+
        ndesc = 0
-       
+
        do i = 1, nsub
           if (associated(p%descendants(i)%ptr)) then
              ndesc = ndesc + 1
              desc(ndesc)%ptr => p%descendants(i)%ptr
           end if
        end do
-       
+
        p%more => desc(1)%ptr
        desc(ndesc+1)%ptr => n
 
        do i = 1, ndesc
           call thread_tree(desc(i)%ptr, desc(i+1)%ptr)
        end do
-       
+
     end select
-    
+
   end subroutine thread_tree
 
-  
+
   !*****************************************************************************
   ! Descend tree to find cells center-of-mass, and sets critical cell radii
   !*****************************************************************************
@@ -303,7 +313,7 @@ contains
 
   end subroutine eval_center_of_mass
 
-  
+
   !*****************************************************************************
   ! Assign critical radius of cell, which determines force evaluation accuracy
   ! TODO: make this a function?
@@ -331,7 +341,7 @@ contains
        d = norm2(p%pos - cm_pos)
        p%rcrit2 = (psize / tree%theta + d)**2
     end if
-    
+
   end subroutine set_critical_radius
 
 
@@ -362,12 +372,12 @@ contains
 
     tree%rsize = rsize
     tree%root%length = rsize
-    
+
   end subroutine expand_box
 
 
   !perhaps I should have a descendant getter instead of accessing the array directly
-  
+
   !*****************************************************************************
   ! Recursively descend tree and find the quadrupole moment of
   ! all cells
@@ -411,13 +421,13 @@ contains
        type is (cell)
           tmpm = tmpm + q%quad_moment
        end select
-       
+
        p%quad_moment = p%quad_moment + tmpm
     end do
 
   end subroutine eval_quadrupole_moment
 
-  
+
   !*****************************************************************************
   ! Outer product of two vectors
   !*****************************************************************************
@@ -431,10 +441,10 @@ contains
           res(j,i) = u(j) * v(i)
        end do
     end do
-    
+
   end function outer_product
 
-  
+
   !*****************************************************************************
   ! Creates an identity matrix
   !*****************************************************************************
@@ -447,7 +457,7 @@ contains
     do k = 1, ndims
        I(k,k) = 1.0_prec
     end do
-    
+
   end function identity
 
 
@@ -456,7 +466,6 @@ contains
   !*****************************************************************************
   subroutine gravcalc(tree)
     class(bhtree), intent(inout) :: tree
-    !    real(real64) :: cpustart, cpuend, cputime
     integer(int64) :: cpustart, cpuend, cputime, clock_count
     real(prec) :: rmid(ndims)
 
@@ -472,7 +481,7 @@ contains
     do i = 1, tree%actlen
        allocate(interact(i)%ptr)
     end do
-    
+
 
     call system_clock(cpustart)
     tree%actmax = 0
@@ -484,13 +493,13 @@ contains
 
     call walk_tree(tree, active, 1, 2, interact, 1, tree%actlen, tree%root, tree%rsize, rmid)
     call system_clock(cpuend, count_rate = clock_count)
-    
+
     cputime = cpuend - cpustart
     if (tree%print_time) print *, "time: ", cputime / real(clock_count)
-    
+
     deallocate(active)
     deallocate(interact)
-    
+
   end subroutine gravcalc
 
 
@@ -592,19 +601,19 @@ contains
     ! just null.
     !*****************************************************************************
     logical function should_loop(q, ap) result(loop)
-    class(node), pointer :: q, ap
-    if (.not. associated(q) .and. .not. associated(ap)) then
-       loop = .false.
-    else if (associated(q, ap)) then
-       loop = .false.
-    else if (.not. associated(q, ap)) then
-       loop = .true.
-    end if
-  end function should_loop
+      class(node), pointer :: q, ap
+      if (.not. associated(q) .and. .not. associated(ap)) then
+         loop = .false.
+      else if (associated(q, ap)) then
+         loop = .false.
+      else if (.not. associated(q, ap)) then
+         loop = .true.
+      end if
+    end function should_loop
 
   end subroutine walk_tree
 
- 
+
   !*****************************************************************************
   ! Calculates the acceleration and potential on a body using the interaction list
   !*****************************************************************************
@@ -618,24 +627,24 @@ contains
 
     phi0_bb = 0.0_prec
     phi0_bc = 0.0_prec
-    
+
     acc0_bb = 0.0_prec
     acc0_bc = 0.0_prec
 
-    !!$omp parallel sections shared(tree, interact, p0, cptr, bptr) &
-    !!$omp& firstprivate(phi0_bc, acc0_bc, phi0_bb, acc0_bb) num_threads(2) &
-    !!$omp& lastprivate(phi0_bc, acc0_bc, phi0_bb, acc0_bb)
-    !!$omp section
+!!$omp parallel sections shared(tree, interact, p0, cptr, bptr) &
+!!$omp& firstprivate(phi0_bc, acc0_bc, phi0_bb, acc0_bb) num_threads(2) &
+!!$omp& lastprivate(phi0_bc, acc0_bc, phi0_bb, acc0_bb)
+!!$omp section
     if (tree%use_quad) then
        call sum_cell(tree, interact(:cptr-1), p0, phi0_bc, acc0_bc)
     else
        call sum_node(tree, interact(:cptr-1), p0, phi0_bc, acc0_bc)
     end if
 
-    !!$omp section
+!!$omp section
     call sum_node(tree, interact(bptr+1:), p0, phi0_bb, acc0_bb)
-    !!$omp end parallel sections
-    
+!!$omp end parallel sections
+
     p0%phi = phi0_bc + phi0_bb
     p0%acc = acc0_bc + acc0_bb
 
@@ -668,7 +677,7 @@ contains
        mr3i = phi_p / dr2
        acc0 = acc0 + (dr * mr3i)
     end do
-    
+
   end subroutine sum_node
 
 
@@ -685,7 +694,7 @@ contains
     real(prec) :: dr2, drab, phi_p, mr3i, drqdr, dr5i, phi_q
     type(cell), pointer :: p
     integer :: i
-    
+
     do i = 1, size(interact) 
        p => interact(i)%ptr
        dr = p%pos - body0%pos
